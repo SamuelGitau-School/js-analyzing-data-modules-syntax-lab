f89da42efc7ddc19fa849980fc6c0d2ade388f16
SamuelGitau_School <samuel.gitau4@student.moringaschool.com>
2026-06-29 14:51:33 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
+function combineUsers(...args){
+  return args.flat
 
+  let combinedObject= {
+    users: []
+  };
+
+  for(let i=0;i<args.length;i++){
+    let currentArray = args[i]
+  }
+  
+  combinedObject.users.push (...combineUsers)
+  combinedObject.merge_date( Date.today().toString('M/d/yyyy'))
+  
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+}
